8b6119a604d6c99ef6075a932a879aa34430dc4e
unknown <mggamota@gmail.com>
2024-11-10 15:42:08 -0600

    Adding code

---
+++ Fob_Code/Debug/Core/Src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/cc1101.c \
+../Core/Src/gpio.c \
+../Core/Src/main.c \
+../Core/Src/spi.c \
+../Core/Src/stm32c0xx_hal_msp.c \
+../Core/Src/stm32c0xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32c0xx.c \
+../Core/Src/usart.c 
+
+C_DEPS += \
+./Core/Src/cc1101.d \
+./Core/Src/gpio.d \
+./Core/Src/main.d \
+./Core/Src/spi.d \
+./Core/Src/stm32c0xx_hal_msp.d \
+./Core/Src/stm32c0xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32c0xx.d \
+./Core/Src/usart.d 
+
+OBJS += \
+./Core/Src/cc1101.o \
+./Core/Src/gpio.o \
+./Core/Src/main.o \
+./Core/Src/spi.o \
+./Core/Src/stm32c0xx_hal_msp.o \
+./Core/Src/stm32c0xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32c0xx.o \
+./Core/Src/usart.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32C011xx -c -I../Core/Inc -IC:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc -IC:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/Legacy -IC:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include -IC:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/cc1101.cyclo ./Core/Src/cc1101.d ./Core/Src/cc1101.o ./Core/Src/cc1101.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/stm32c0xx_hal_msp.cyclo ./Core/Src/stm32c0xx_hal_msp.d ./Core/Src/stm32c0xx_hal_msp.o ./Core/Src/stm32c0xx_hal_msp.su ./Core/Src/stm32c0xx_it.cyclo ./Core/Src/stm32c0xx_it.d ./Core/Src/stm32c0xx_it.o ./Core/Src/stm32c0xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32c0xx.cyclo ./Core/Src/system_stm32c0xx.d ./Core/Src/system_stm32c0xx.o ./Core/Src/system_stm32c0xx.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su
+
+.PHONY: clean-Core-2f-Src
+
---
+++ Fob_Code/Debug/Drivers/STM32C0xx_HAL_Driver/stm32c0xx_hal_flash.d
@@ -0,0 +1,62 @@
+Drivers/STM32C0xx_HAL_Driver/stm32c0xx_hal_flash.o: \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Src/stm32c0xx_hal_flash.c \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_ll_system.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c011xx.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h \
+ ../Core/Inc/stm32c0xx_hal_conf.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_rcc.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_def.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_ll_rcc.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_rcc_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_gpio.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_gpio_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_dma.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_dma_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_cortex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_exti.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_flash.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_flash_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_pwr.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_pwr_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_spi.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_spi_ex.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_uart.h \
+ C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_uart_ex.h
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_ll_system.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c0xx.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/stm32c011xx.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/core_cm0plus.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/CMSIS/Device/ST/STM32C0xx/Include/system_stm32c0xx.h:
+../Core/Inc/stm32c0xx_hal_conf.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_rcc.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_def.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_ll_rcc.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_rcc_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_gpio.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_gpio_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_dma.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_dma_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_cortex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_exti.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_flash.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_flash_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_pwr.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_pwr_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_spi.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_spi_ex.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_uart.h:
+C:/Users/mggam/STM32Cube/Repository/STM32Cube_FW_C0_V1.2.0/Drivers/STM32C0xx_HAL_Driver/Inc/stm32c0xx_hal_uart_ex.h:
